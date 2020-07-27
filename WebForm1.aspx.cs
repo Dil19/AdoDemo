7919109e4c12ff06e7324e4b2f08db2683f07db7
Dil19 <59259863+Dil19@users.noreply.github.com>
2020-07-27 12:53:45 +0530

    first commit

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.OleDb;
+using System.Data.Odbc;
+
+namespace AdoDemo001
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+            /* SqlConnection con = new SqlConnection(CS);
+             try
+             {
+                 SqlCommand cmd = new SqlCommand("Select * from tblProduct", con);
+                 con.Open();
+                 SqlDataReader rdr = cmd.ExecuteReader();
+                 GridView1.DataSource = rdr;
+                 GridView1.DataBind();
+             }
+             catch
+             {
+
+             }
+             finally
+             {
+                 con.Close();
+             }*/
+
+
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from tblProduc", con);
+                con.Open();
+                GridView1.DataSource = cmd.ExecuteReader();
+                GridView1.DataBind();
+                con.Close();
+            }
+
+
+        }
+    }
+}
